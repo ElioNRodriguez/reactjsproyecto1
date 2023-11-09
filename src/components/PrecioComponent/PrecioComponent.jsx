3ac2aaf3964082c5495d703247ffb875faf5f0db
Elio Rodriguez <rodriguezelio2@gmail.com>
2023-11-08 22:58:04 -0300

    Se agrega CardComponent, PrecioComponent, Stock Component, estilos bootstrap

---
+++ src/components/PrecioComponent/PrecioComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let precio = 400.000
+const PrecioComponent = () => {
+    return (
+        <>
+            <b>Precio: {precio}</b>
+        </>
+    );
+}
+
+export default PrecioComponent;
